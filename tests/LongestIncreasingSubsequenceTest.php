9c467cd40b7a7e629c510222cf458225e8584c8f
roopa <roopa.polavaram@gmail.com>
2022-01-23 18:29:50 +1100

    Add test to validate when sequence has more than one max

--- tests/LongestIncreasingSubsequenceTest.php
+++ tests/LongestIncreasingSubsequenceTest.php
@@ -11,7 +11,11 @@ class LongestIncreasingSubsequenceTest extends TestCase {
 
     public function testArraySequence() : void {             
         self::assertSame("1 6", LongestIncreasingSubsequence::process('9 1 6 4 3 2'));
-    }    
+    }
+
+    public function testArraySequenceWithMoreThanOneMax() : void {             
+        self::assertSame("1 5", LongestIncreasingSubsequence::process('9 1 5 4 3 2 1 8'));
+    }
     
 
 }
